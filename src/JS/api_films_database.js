1d1dd878e435eeb33e0a9f186199e15b99f51041
tomaspalica <tomasss.palica@gmail.com>
2023-07-22 12:01:04 +0200

    add and fix
    zmieniłem funkcje na async/await oraz dodałem fetch objektu wszystkich klas i fetch filmów przez ich id

--- src/JS/api_films_database.js
+++ src/JS/api_films_database.js
@@ -3,42 +3,67 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500/"
 const BASE_URL = "https://api.themoviedb.org/3"
 const API_URL = BASE_URL + '/trending/movie/day' + MY_KEY
 const searchURL = BASE_URL + '/search/movie' + MY_KEY
-const fetchMovies = (url) => {
-    return fetch(url)
-    .then(response => {
-      
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      console.log(response)
-      return response.json();
-    })
-    .then(data => {
-      if (data.length === 0) {
-        throw new Error('No movie data available');
-      }
-      console.log(data)
-      return data;
-    });
+const fetchMovies = async () => {
+  
+  const response = await fetch(API_URL)
+if (!response.ok) {
+  throw new Error('Request failed')
 }
-const fetchMoviesByName = (searchValue) => {
-    const url = `${searchURL}&query=${encodeURIComponent(searchValue)}`
+console.log(response)
+const data = await response.json()
+if (data.length === 0) {
+  throw new Error('No movie data available')
+}
+console.log(data)
+return data
+}
 
-    return fetch(url)
-    .then(response => {
-      
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      console.log(response)
-      return response.json();
-    })
-    .then(data => {
-      if (data.length === 0) {
-        throw new Error('No movie data available');
-      }
-      console.log(data)
-      return data;
-    });
+const fetchMoviesByName = async (searchValue) => {
+  const url = `${searchURL}&query=${encodeURIComponent(searchValue)}`
+
+  const response = await fetch(url)
+   if (!response.ok) {
+     throw new Error('Request failed')
+   }
+   console.log(response)
+   const data = await response.json()
+   if (data.length === 0) {
+     throw new Error('No cat data available')
+   }
+   console.log(data)
+   return data
 }
-export {fetchMovies,fetchMoviesByName}
+// ---------------------- kategorie filmów --------------------------------------------
+const fetchGenres = async () => {
+    
+
+  const response = await fetch(GENRE_URL)
+if (!response.ok) {
+  throw new Error('Request failed')
+}
+console.log(response)
+const data = await response.json()
+if (data.length === 0) {
+  throw new Error('No movie data available')
+}
+console.log(data)
+return data
+}
+// -------------------- szukanie filmów przez id -------------------------------------
+const fetchMoviesByID = async (id) => {
+  const url = BASE_URL + `/movie/${id}` + MY_KEY
+
+const response = await fetch(url)
+if (!response.ok) {
+throw new Error('Request failed')
+}
+console.log(response)
+const data = await response.json()
+if (data.length === 0) {
+throw new Error('No movie data available')
+}
+console.log(data)
+return data
+}
+
+export {fetchMovies,fetchMoviesByName, fetchGenres, fetchMoviesByID}
