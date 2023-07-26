849515c1580cc1716ddd9ae93ee04be6e75baa83
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-07-26 20:49:00 +0200

    2023-07-24/add/film_cards.js

--- src/JS/film_cards.js
+++ src/JS/film_cards.js
@@ -1,24 +1,21 @@
+const gallery = document.querySelector('.section-films');
+
 function createFilmCards(films) {
-  const markupImages = films.results
+  const markupImages = films
     .map(
-      film =>
-        `  
-        <div class="film-card">
-            <a href=<img class="film-card__img" src="${IMG_URL}${film.poster_path}" alt="${
-          film.overview
-        }" title="${film.title}" ID="${film.id}" loading="lazy"/></a>
-            <div class="film-card__info">
-                <p class="film-card__info-item">
-                    <b>Title: </b>${film.title}
-                </p>
-                <p class="film-card__info-item">
-                    <b>Genre: </b>${film.genre}
-                </p>
-                <p class="film-card__info-item">
-                    <b>Year: </b>${film.release_date.split('-')[0]}
-                </p>
-            </div>
-        </div>`,
+      film => `<div class="film-card">
+        <img class="film-card__img" src="${IMG_URL}${film.poster_path}" alt="${
+        film.overview
+      }" title="${film.title}" ID="${film.id}" loading="lazy"/>
+        <div class="film-card__info">
+            <p class="film-card__info--title">
+            ${film.title}
+            </p>
+            <p class="film-card__info--subtitle">
+            ${film.genre} | ${film.release_date.split('-')[0]}
+            </p>
+        </div>
+    </div>`,
     )
     .join(' ');
   gallery.innerHTML = markupImages;
