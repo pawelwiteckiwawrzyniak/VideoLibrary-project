fed63b51db775745476a86dfca7fca0a7fab5b9c
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-07-30 10:49:32 +0200

    repair repository

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
- /************************************************************************************************************************************************/
+/************************************************************************************************************************************************/
 import * as api from './JS/api_films_database';
 //import * as filmsCard from './JS/film_cards';
-//import * as functionsProject from './JS/info_functions';
+import * as functionsProject from './JS/info_functions';
 import * as modalCardFilm from './JS/modal_card_film';
- import './sass/main.scss';
+import './sass/main.scss';
 
 // /************************************************************************************************************************************************/
-// const searchForm = document.querySelector('#search-form');
-// const input = document.querySelector('.search-form__input');
-// const cardsFilm = document.querySelector('.section-films');
+const searchForm = document.querySelector('#search-form');
+const input = document.querySelector('.search-form__input');
+const cardsFilm = document.querySelector('.section-films');
 
 // // const spinner = document.querySelector('.loader__div');
 
 // /************************************************************************************************************************************************/
-// searchForm.addEventListener('submit', searchFilms);
-// cardsFilm.addEventListener('click', openCardFilm);
+searchForm.addEventListener('submit', searchFilms);
+cardsFilm.addEventListener('click', openCardFilm);
 
 // /************************************************************************************************************************************************/
 // async function searchFilms(eve) {
