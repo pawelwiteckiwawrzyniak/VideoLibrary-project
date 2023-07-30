82f51b2a093ecba7184b6af049da2cc7f5798dc0
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-07-30 12:53:22 +0200

    repair repository

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import * as api from './JS/api_films_database';
 import * as filmsCard from './JS/film_cards';
 import * as functionsProject from './JS/info_functions';
 import * as modalCardFilm from './JS/modal_card_film';
+import * as studentsInfo from './JS/modal_footer';
 import './sass/main.scss';
 
 /************************************************************************************************************************************************/
 const searchForm = document.querySelector('#search-form');
 const input = document.querySelector('.search-form__input');
 const cardsFilm = document.querySelector('.section-films');
-
+//const studentsModal = document.querySelector('#studentsModal');
 // const spinner = document.querySelector('.loader__div');
 
 /************************************************************************************************************************************************/
 searchForm.addEventListener('submit', searchFilms);
 cardsFilm.addEventListener('click', openCardFilm);
-
+//studentsModal.addEventListener('click', openCardFilm);
 /************************************************************************************************************************************************/
 async function searchFilms(eve) {
   eve.preventDefault();
